c68dff46556b88ad2ef3b6e634c7f7c4a1be982e
UnaiUrti <unaiurti@gmail.com>
2021-11-10 22:05:32 +0100

    Clase main de la aplicacion

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package client;
 
+import client.controllers.VSignInController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
 /**
- *
- * @author irkus
+ * Clase principal la cual va a abrir la primera ventana SignIn
+ * @author Unai Urtiaga
  */
-public class Client {
+public class Client extends Application{
 
     /**
+     * Metodo main que va a ejecutar el metodo start
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
-    
+
+    /**
+     * Metodo start para abrir la primera ventana de la aplicacion
+     * @param primaryStage Parametro de tipo Stage el cual se va a utilizar para
+     * darle un nuevo stage al controlador
+     * @throws Exception Un controlador para la excepcion del load()
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/client/controllers/VSignIn.fxml"));
+        
+        Parent root = loader.load();
+        
+        VSignInController controller = ((VSignInController)loader.getController());
+        controller.setStage(primaryStage);
+        controller.initStage(root);
+        
+    }
 }
