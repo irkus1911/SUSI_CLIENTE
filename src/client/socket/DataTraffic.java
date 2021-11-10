47ce7cc8e8bc36807e07301c853dde1c42f19035
irkus1911 <irkus1911@irkusfuga@gmail.com>
2021-11-10 19:44:58 +0100

    Sign up sin excepciones

--- src/client/socket/DataTraffic.java
+++ src/client/socket/DataTraffic.java
@@ -24,7 +24,7 @@ import static lib.message.Msg.PASSWORDDONTMATCHEXCEPTION;
 public class DataTraffic implements Logicable {
 
     /**
-     *  *  Este metodo recibe un objeto usuario y lo convierte en el objeto mensaje  asignandole el signIn a realizar a parte tambien traduce el mensaje de vuelta del servidor lanzando las diferentes excepciones
+     *  Este metodo recibe un objeto usuario y lo convierte en el objeto mensaje  asignandole el signIn a realizar a parte tambien traduce el mensaje de vuelta del servidor lanzando las diferentes excepciones
      * @param user Objeto usuario recibido mediante el socket 
      * @return objeto User Devuelve el usuario en caso de no encontrar nada devuelve nulo
      * @throws IncorrectUserException El usuario no es alfanumerico
@@ -43,29 +43,64 @@ public class DataTraffic implements Logicable {
         msg.setUser(user);
         msg.setMsg(Msg.SIGNIN);
 
-      
+       
             //Enviar el mensaje al servidor
             msg = socket.createSocket(msg);
-        
+     
             //Error de conexion con el servidor
-            throw new ConnectException("Error de conexion, intentalo mas tarde");
-       
+        
         //Traducir el mensaje de vuelta
-        
+       
             ///Usuario no existe
-            throw new UserDontExistException("El usuario no existe");
-        
+            
             //Error de conexion
-            throw new ConnectException("Error al conectarse con la base de datos");
-        
+       
             //Contraseña no es la del usuario
-            throw new PasswordDontMatchException("La contraseña no coincide");
-        
+     
             //Error demasiadas conexiones
           
         //Devolver el usuario
         return msg.getUser();
     }
 
-   
+   /**
+     * Este metodo recibe un objeto usuario y lo convierte en el objeto mensaje  asignandole el signUp a realizar a parte tambien traduce el mensaje de vuelta del servidor lanzando las diferentes excepciones
+     * @param user Objeto usuario recibido mediante el socket 
+     * @return objeto User Devuelve el usuario creado
+     * @throws IncorrectUserException El usuario no es alfanumerico
+     * @throws IncorrectPasswordException La contraseña no es alfanumerica
+     * @throws IncorrectEmailException Patron de correo incorrecto 
+     * @throws UserExistException   Usuario ya existe en la base de datos
+     * @throws PasswordDontMatchException Las contraseñas no coinciden entre si(contraseña y confirmar contraseña)
+     * @throws ConnectException Hay un error de conexion con la base de datos
+     */
+     //SignUp  Recibe Usuario/Devuelve Usuario
+    @Override
+    public User signUp(User user) throws IncorrectUserException, IncorrectPasswordException, IncorrectEmailException, UserExistException, PasswordDontMatchException, ConnectException, TooManyUsersException {
+        //Crear socket cliente
+        ClientSocket socket = new ClientSocket();
+        //Asignarle mensaje(metodo+user)
+        Message msg = new Message();
+        msg.setUser(user);
+        msg.setMsg(Msg.SIGNUP);
+
+      
+            //Enviar el mensaje al servidor
+            msg = socket.createSocket(msg);
+       
+            //Error conexion con el servidor
+     
+        //Traducir el mensaje de vuelta
+    
+            //Usuario ya existe
+        
+            //Error de conexion
+         
+            //Error contraseñas no coinciden
+        
+            //Error superado el limite de conexiones
+       
+        //Devolver el usuario
+        return msg.getUser();
+    }
 }
