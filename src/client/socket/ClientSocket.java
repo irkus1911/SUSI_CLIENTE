41cf1e152c692aba148f374c35aee51291f68d2c
19adrii28 <adrianfrancorodriguez021@gmail.com>
2021-11-11 09:30:43 +0100

    Socket Arreglado, Throws añadidos.

--- src/client/socket/ClientSocket.java
+++ src/client/socket/ClientSocket.java
@@ -12,60 +12,69 @@ import java.net.Socket;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import lib.dataModel.User;
 import lib.message.Message;
-import lib.message.Msg;
+import lib.exceptions.ConnectException;
+import lib.exceptions.TooManyUsersException;
 
 /**
  *Esta clase implementa la Interfaz
  * @author Adrian Franco
- * 
  */
 public class ClientSocket {
 
+    private final static Logger logger = Logger.getLogger("client.socket.ClientSocket");
     private ResourceBundle configFile;
-    private String port;
-    private int ip;
-    private final static int PORT = 5001;
-    private final static String SERVER = "127.0.0.1";
+    private String SERVER;
+    private int PORT;
 
     /**
-     * @param args Usuario recibido
+     *
+     * @param msg Objeto mensaje que se envia al servidor
      * @return Objeto Message recibido de Server con los usuarios,
      * datos y excepciones que hayan sido utlizadas
+     * @throws ConnectException Excepcion que salta al conectarse con la base 
+     * de datos
+     * @throws TooManyUsersException excepcion para cuando el Usuario se pase
+     * del limite de usuarios
+     * @throws IOException Excepcion por la cual sacamos el Error de conexion
      */
+    public Message createSocket(Message msg) throws ConnectException, TooManyUsersException, IOException {
 
-    public Message createSocket(Message msg) {
-    /**
-     * 
-     * Metodo en el que abrimos la conexion con el cliente y le pasamos la 
-     * clase Message
-     */
+        readFile();
         Message mg = null;
-        Socket socket = null;
         ObjectOutputStream oos = null;
         ObjectInputStream ois = null;
+        Socket socket = null;
 
         try {
             socket = new Socket(SERVER, PORT);
+            logger.info("Conectado con el servidor");
             oos = new ObjectOutputStream(socket.getOutputStream());
             ois = new ObjectInputStream(socket.getInputStream());
-
             oos.writeObject(msg);
+            logger.info("Se ha enviado un objeto al servidor");
             mg = (Message) ois.readObject();
+            logger.info("Leyendo el objeto devuelto por el servidor");
+            ois.close();
+            oos.close();
             socket.close();
+            logger.info("Se ha cerrado el socket del cliente");
         } catch (IOException ex) {
-
+            logger.info("Error de conexion");
+            throw new ConnectException("Error de conexion, intentalo mas tarde");          
         } catch (ClassNotFoundException ex) {
-
+            logger.info("");
         }
         return mg;
-
     }
 
+    /**
+     *
+     */
     public void readFile() {
         this.configFile = ResourceBundle.getBundle("client.socket.ClientProperties");
-        this.ip = Integer.parseInt(this.configFile.getString("IP"));
-        this.port = this.configFile.getString("PORT");
+        this.SERVER = this.configFile.getString("SERVER");
+        this.PORT = Integer.parseInt(this.configFile.getString("PORT"));
     }
+
 }
